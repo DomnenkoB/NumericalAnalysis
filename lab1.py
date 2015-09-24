41f166a24b9c2be34d8f55d2d5e19933e0b2d6fc
DomnenkoB <papadoble151@gmail.com>
2015-09-24 15:21:07 +0800

    lab1

---
+++ lab1.py
@@ -0,0 +1,85 @@
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+	return np.power(2, x) + x * x - 1.15
+
+def d_f(x):
+	return np.log(2) * np.power(2, x) + 2 * x
+
+def alpha(x):
+	return (-1.0) / d_f(x)
+
+def newton(a, b, eps):
+	x = random.uniform(a, b)
+
+	#convergence_crt
+
+	if (math.fabs(1.0 + alpha(x) * d_f(x))):
+		print('Method diverges, x value:')
+		print(x)
+	else:
+		# in order to iterate we decrease the value of x_n
+		x_n = x
+		x_n -= 2 * eps
+		iter_cnt = 0
+
+		#'iteration' process
+
+		while math.fabs(x_n - x) > eps:
+			x = x_n
+			x_n = x + f(x) * alpha(x)
+			iter_cnt += 1
+
+			print('Iteration: ' + str(iter_cnt) + ', x value:')
+			print(x_n)
+
+		print('Answer:')
+		print((x_n))
+
+def modified_newton(a, b, eps):
+	x = random.uniform(a, b)
+
+	#convergence_crt
+
+	if (math.fabs(1.0 + alpha(x) * d_f(x))):
+		print('Method diverges, x value:')
+		print(x)
+	else:
+		# in order to iterate we decrease the value of x_n
+		x_n = x
+		x_n -= 2 * eps
+		iter_cnt = 0
+
+		#'iteration' process
+
+		while math.fabs(x_n - x) > eps:
+			x = x_n
+			x_n = x - (f(x) * eps) / (f(x + eps) - f(x))
+			iter_cnt += 1
+			print('Iteration: ' + str(iter_cnt) + ', x value:')
+			print(x_n)
+
+		print('Answer:')
+		print((x_n))
+
+
+newton(-1, 1, 0.001)
+modified_newton(-1, 1, 0.001)
+
+
+x = np.arange(-4, 4, 0.01)
+
+plt.title('Funtion 2^x + x*x - 1.15')
+plt.grid(True)
+plt.ylim(-8, 8)
+plt.plot(x, f(x))
+plt.show()
+
+plt.title('Funtion 2^x + x*x - 1.15')
+plt.grid(True)
+plt.ylim(-8, 8)
+plt.plot(x, d_f(x))
+plt.show()
